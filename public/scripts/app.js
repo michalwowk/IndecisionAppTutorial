b430b88bc9810486ef6ef6d73567aa2414f86cd0
Michał Wowk <michalwowk92@gmail.com>
2018-04-03 22:54:28 +0200

    feat: JSX Expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,53 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
+
+var app = {
+    title: 'Indecision Title',
+    subtitle: 'My first R eact app'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Dont give up'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
 );
+
+var user = {
+    name: 'Michal',
+    age: '25',
+    location: 'Michalowo'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        user.location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Dont give up</h1>;
+
+var app = {
+    title: 'Indecision Title',
+    subtitle: 'My first R eact app'
+};
+
+var template =(
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+
+    </div>
+);
+
+var user = {
+    name: 'Michal',
+    age: '25',
+    location: 'Michalowo'
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>{user.age}</p>
+        <p>{user.location}</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
