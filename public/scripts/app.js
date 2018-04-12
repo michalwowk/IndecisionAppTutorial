41016c7139e411f79e3e5f6d522f9ed1a7393ed4
Unknown <michalwowk92@gmail.com>
2018-04-12 22:14:03 +0200

    feat: Practising es6 features

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,8 @@ console.log('App.js is running!');
 
 var app = {
     title: 'Indecision Title',
-    subtitle: 'My first R eact app'
+    subtitle: 'My first R eact app',
+    options: ['One', 'Two']
 };
 
 var template = React.createElement(
@@ -17,10 +18,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     )
 );
 
@@ -30,26 +36,58 @@ var user = {
     location: 'Michalowo'
 };
 
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymouse'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
+        'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+// const firstName = (tatata) => {
+//     return tatata.split(' ')[0];
+// }
+
+// const firstNameArrow = (name) => name.split(' ')[0];
+
+// console.log(firstName('full Name'))
+// console.log(firstNameArrow('ula la laal'))
+
+
+var multipier = {
+    numbers: [1, 2, 3, 4, 5, 6],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this.multiplyBy;
+        });
+    }
+};
+
+console.log(multipier.multiply());
--- src/app.js
+++ src/app.js
@@ -2,34 +2,60 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 
-var app = {
+const app = {
     title: 'Indecision Title',
-    subtitle: 'My first R eact app'
+    subtitle: 'My first R eact app',
+    options: ['One','Two']
 };
 
-var template =(
+const template =(
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
-
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     </div>
 );
 
-var user = {
+const user = {
     name: 'Michal',
     age: '25',
     location: 'Michalowo'
 }
 
-var templateTwo = (
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+const templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>{user.age}</p>
-        <p>{user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymouse'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
 
+// const firstName = (tatata) => {
+//     return tatata.split(' ')[0];
+// }
+
+// const firstNameArrow = (name) => name.split(' ')[0];
+
+// console.log(firstName('full Name'))
+// console.log(firstNameArrow('ula la laal'))
+
+
+const multipier = {
+    numbers: [1,2,3,4,5,6],
+    multiplyBy: 2,
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy);
+    }
+};
+
+console.log(multipier.multiply());
